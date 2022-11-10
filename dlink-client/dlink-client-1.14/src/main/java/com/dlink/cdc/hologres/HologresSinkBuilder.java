698fd4b1dfc4b4835d190c7f6850cbc7e489a325
booleandev <jiantao.yan@qq.com>
2022-11-10 10:56:58 +0800

    feat: add hologres metadata11

---
+++ dlink-client/dlink-client-1.14/src/main/java/com/dlink/cdc/hologres/HologresSinkBuilder.java
@@ -0,0 +1,10 @@
+package com.dlink.cdc.hologres;
+
+/**
+ * @className: HologresSinkBuilder
+ * @description: TODO 类描述
+ * @author: Jiantao Yan
+ * @date: 2022-11-8 17:50
+ **/
+public class HologresSinkBuilder {
+}
